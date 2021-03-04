807977d8f331a59f89c27d6b4dd7aa6ef62c28bd
Simon Brown <simon@structurizr.com>
2021-03-04 16:37:44 +0000

    Fixes a broken test.

--- src/test/java/com/structurizr/dsl/StructurizrDslFormatterTests.java
+++ src/test/java/com/structurizr/dsl/StructurizrDslFormatterTests.java
@@ -148,7 +148,7 @@ class StructurizrDslFormatterTests extends AbstractTests {
                 "\n" +
                 "        enterprise \"Big Bank plc\" {\n" +
                 "            CustomerServiceStaff = person \"Customer Service Staff\" \"Customer service staff within the bank.\" \"Bank Staff\" \n" +
-                "            BackOfficeStaff = person \"Back Office Staff\" \"Administration and support staff within the bank.\" \"\" \n" +
+                "            BackOfficeStaff = person \"Back Office Staff\" \"Administration and support staff within the bank.\" \"Bank Staff\" \n" +
                 "            MainframeBankingSystem = softwareSystem \"Mainframe Banking System\" \"Stores all of the core banking information about customers, accounts, transactions, etc.\" \"Existing System\" \n" +
                 "            EmailSystem = softwareSystem \"E-mail System\" \"The internal Microsoft Exchange e-mail system\" \"Existing System\" \n" +
                 "            ATM = softwareSystem \"ATM\" \"Allows customers to withdraw cash.\" \"Existing System\" \n" +
