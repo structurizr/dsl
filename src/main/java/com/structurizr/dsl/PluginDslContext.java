c50dfd9d62cb1ca4f890e7b5bfc123165d7c03cb
Simon Brown <simon@structurizr.com>
2022-01-31 14:23:53 +0000

    Allow plugins to be used off the regular classpath, without the need for an empty plugins directory.

--- src/main/java/com/structurizr/dsl/PluginDslContext.java
+++ src/main/java/com/structurizr/dsl/PluginDslContext.java
@@ -25,29 +25,27 @@ class PluginDslContext extends DslContext {
     void end() {
         String fqn = fullyQualifiedClassName;
         try {
-            if (!pluginsDirectory.exists()) {
-                throw new RuntimeException("No plugins directory found");
-            }
+            URL[] urls = new URL[0];
 
-            File[] jarFiles = pluginsDirectory.listFiles((dir, name) -> name.endsWith(".jar"));
-            if (jarFiles != null) {
-                URL[] urls = new URL[jarFiles.length];
-                for (int i = 0; i < jarFiles.length; i++) {
-                    try {
-                        urls[i] = jarFiles[i].toURI().toURL();
-                    } catch (Exception e) {
-                        e.printStackTrace();
+            if (pluginsDirectory.exists()) {
+                File[] jarFiles = pluginsDirectory.listFiles((dir, name) -> name.endsWith(".jar"));
+                if (jarFiles != null) {
+                    urls = new URL[jarFiles.length];
+                    for (int i = 0; i < jarFiles.length; i++) {
+                        try {
+                            urls[i] = jarFiles[i].toURI().toURL();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
                     }
                 }
-
-                URLClassLoader childClassLoader = new URLClassLoader(urls, getClass().getClassLoader());
-
-                StructurizrDslPlugin plugin = (StructurizrDslPlugin)Class.forName(fqn, true, childClassLoader).getDeclaredConstructor().newInstance();
-                StructurizrDslPluginContext pluginContext = new StructurizrDslPluginContext(getWorkspace(), parameters);
-                plugin.run(pluginContext);
-            } else {
-                throw new RuntimeException("No JAR files found in " + pluginsDirectory.getCanonicalPath());
             }
+
+            URLClassLoader childClassLoader = new URLClassLoader(urls, getClass().getClassLoader());
+
+            StructurizrDslPlugin plugin = (StructurizrDslPlugin)Class.forName(fqn, true, childClassLoader).getDeclaredConstructor().newInstance();
+            StructurizrDslPluginContext pluginContext = new StructurizrDslPluginContext(getWorkspace(), parameters);
+            plugin.run(pluginContext);
         } catch (Exception e) {
             e.printStackTrace();
             throw new RuntimeException("Error running plugin " + fqn + ", caused by " + e.getClass().getName() + ": " + e.getMessage());
