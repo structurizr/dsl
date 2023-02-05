2cae88eb722379389c61bbb47fb13f7e65b07a5a
Simon Brown <simon@structurizr.com>
2023-02-05 16:48:25 +0000

    Remove DSL formatter ... this was only a temporary thing to help people migrate from the old workspace editor.

--- src/main/java/com/structurizr/dsl/StructurizrDslFormatterException.java
+++
@@ -1,9 +0,0 @@
-package com.structurizr.dsl;
-
-public final class StructurizrDslFormatterException extends Exception {
-
-    StructurizrDslFormatterException(String message) {
-        super(message);
-    }
-
-}
