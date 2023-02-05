54e16f69502377c9c824b44472e0da02f32839a2
Simon Brown <simon@structurizr.com>
2023-02-05 16:48:48 +0000

    Add a warning when using automatically generated keys.

--- src/main/java/com/structurizr/dsl/AbstractViewParser.java
+++ src/main/java/com/structurizr/dsl/AbstractViewParser.java
@@ -2,14 +2,17 @@ package com.structurizr.dsl;
 
 import com.structurizr.Workspace;
 import com.structurizr.view.FilteredView;
-import com.structurizr.view.SystemLandscapeView;
 import com.structurizr.view.View;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
 abstract class AbstractViewParser extends AbstractParser {
+
+    private static final Log log = LogFactory.getLog(AbstractViewParser.class);
 
     protected String generateViewKey(Workspace workspace, String prefix) {
         int counter = 1;
@@ -20,6 +23,7 @@ abstract class AbstractViewParser extends AbstractParser {
             key = prefix + "-" + counter;
         }
 
+        log.warn(key + " is an automatically generated view key - you will likely lose manual layout information when using automatically generated view keys.");
         return key;
     }
 
